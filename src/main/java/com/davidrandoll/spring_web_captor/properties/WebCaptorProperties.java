6657ba1562e840746f49d56e34860344c3673edc
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-06-28 17:27:05 -0400

    Add EventDetails to WebCaptorProperties
    Introduced a new EventDetails nested configuration class in WebCaptorProperties to allow fine-grained control over event data capture. Updated application.yaml test config to enable all additional details.

--- src/main/java/com/davidrandoll/spring_web_captor/properties/WebCaptorProperties.java
+++ src/main/java/com/davidrandoll/spring_web_captor/properties/WebCaptorProperties.java
@@ -12,6 +12,9 @@ public class WebCaptorProperties {
     private boolean enabled = true;
 
     @NestedConfigurationProperty
+    private EventDetails eventDetails = new EventDetails();
+
+    @NestedConfigurationProperty
     private AdditionalDetails additionalDetails = new AdditionalDetails();
 
     @Data
@@ -19,5 +22,22 @@ public class WebCaptorProperties {
         private boolean duration = true;
         private boolean ipAddress = true;
         private boolean userAgent = true;
+    }
+
+    @Data
+    public static class EventDetails {
+        private boolean includeFullUrl = true;
+        private boolean includePath = true;
+        private boolean includeMethod = true;
+        private boolean includeRequestHeaders = true;
+        private boolean includeQueryParams = true;
+        private boolean includePathParams = true;
+        private boolean includeRequestBody = true;
+        private boolean includeMultipartFiles = true;
+        private boolean includeResponseHeaders = true;
+        private boolean includeResponseBody = true;
+        private boolean includeResponseStatus = true;
+        private boolean includeErrorDetails = true;
+        private boolean includeEndpointExists = true;
     }
 }
